44298ef124d2283dc3fc4369553b05cb072d9921
won <71067307+won195@users.noreply.github.com>
2023-09-26 21:59:06 +0900

    user

--- user/app.js
+++ user/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const argon2 = require('argon2');
 const app = express();
 
 const database= [{ id: 1, username: 'abc', password: 'abc'}];
@@ -14,17 +15,36 @@ app.get('/users', (req, res) => {
   res.send(database);
 })
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
   const { username, password, age, birthday } = req.body;
+  const hash = await argon2.hash(password);
   database.push({
     username,
-    password,
+    password: hash,
     age,
     birthday,
-  })
+  });
   res.send('success');
 });
 
+app.post('/login', async (req, res) => {
+  const { username,password } = req.body;
+  const user = database.filter((user) => {
+    return user.username === username;
+  });
+  if (user.length === 0) {
+    res.status(403).send('해당하는 ID가 없습니다');
+    return;
+  }
+
+  if (!(await argon2.verify(user[0].password, password))) {
+    res.status(403).send('패스워드가 틀립니다.');
+    return;
+  }
+
+  res.send('로그인 성공')
+});
+
 app.listen(3000, () => {
-  console.log('server on!');
+  console.log('localhost:3000 server on!');
 }); 
